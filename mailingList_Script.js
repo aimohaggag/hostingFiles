c1e319dcc5ee562fc03399e62f630b8f52b33abf
bcdm <33904138+aimohaggag@users.noreply.github.com>
2018-03-30 10:34:39 +0200

    Create mailingList_Script.js
    mailing list script

---
+++ mailingList_Script.js
@@ -0,0 +1,21 @@
+// <![CDATA[
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.4";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+     
+// ]]></script>
+</td>
+</tr>
+</tbody>
+</table>
+</div>
+<p style="text-align: center;">
+<!--<a href="https://twitter.com/bluecircle_diam" class="twitter-follow-button" data-show-count="true" data-show-screen-name="false" data-dnt="true">Follow @bluecircle_diam</a></p>-->
+<script>// <![CDATA[
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+     
+// ]]>
