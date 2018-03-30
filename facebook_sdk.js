734895443f1ac3af4a14087c0e2f56f2f462425c
bcdm <33904138+aimohaggag@users.noreply.github.com>
2018-03-30 14:59:51 +0200

    Create facebook_sdk.js
    facebook_sdk.js

---
+++ facebook_sdk.js
@@ -0,0 +1,12 @@
+
+//<![CDATA[
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/ar_AR/sdk.js#xfbml=1&version=v2.12&appId=358189891326895&autoLogAppEvents=1';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+//]]>
