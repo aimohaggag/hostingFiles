ffdadf1c8a6b7f19aa1b538fe1ef6967d27159da
bcdm <33904138+aimohaggag@users.noreply.github.com>
2018-03-30 15:08:23 +0200

    Create page_navigation1.js
    page_navigation1.js

---
+++ page_navigation1.js
@@ -0,0 +1,12 @@
+
+  /*<![CDATA[*/
+    var numPages=5;
+     var firstText ='الأولى';
+    var lastText ='الأخيرة';
+    var prevText ='السابق';
+  
+  var nextText = 'التالي'  ;
+
+    var urlactivepage=location.href;
+    var home_page="/";
+  /*]]>*/
