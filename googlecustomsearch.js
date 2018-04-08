2886ed051959bcde67fdbb00311d5da8d02e4414
bcdm <33904138+aimohaggag@users.noreply.github.com>
2018-04-08 10:42:31 +0200

    Create googlecustomsearch.js
    google custom search

---
+++ googlecustomsearch.js
@@ -0,0 +1,11 @@
+
+  (function() {
+    var cx = '003201941879784817229:_ac4xlzc2c4';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
+
