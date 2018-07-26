961d7cfbd9d6e59d240fb75c515f9e206642647c
bcdm <33904138+aimohaggag@users.noreply.github.com>
2018-07-26 11:14:08 +0200

    Create disableCopy72018.js

---
+++ disableCopy72018.js
@@ -0,0 +1,11 @@
+ function disableselect(e){
+return false
+}
+function reEnable(){
+return true
+}
+document.onselectstart=new Function (&quot;return false&quot;)
+if (window.sidebar){
+document.onmousedown=disableselect
+document.onclick=reEnable
+}
