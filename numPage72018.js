a807c784d22e7e32dc92ff2766f004b3d180f37f
bcdm <33904138+aimohaggag@users.noreply.github.com>
2018-07-26 11:19:19 +0200

    Create numPage72018.js

---
+++ numPage72018.js
@@ -0,0 +1,10 @@
+  /*<![CDATA[*/
+    
+var numPages=5;
+    var firstText ='الأولى';
+    var lastText ='الأخيرة';
+    var prevText ='السابق ';
+    var nextText =' التالي';;
+    var urlactivepage=location.href;
+    var home_page="/";
+  /*]]>*/
