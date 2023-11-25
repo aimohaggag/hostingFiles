232dce40c65ae0c3cf434000ca674061144b9c98
Amr Haggag <33904138+aimohaggag@users.noreply.github.com>
2023-11-25 20:35:18 +0300

    Update calorie-calculator.js

--- calorie-calculator.js
+++ calorie-calculator.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
   $('#submitButton').click(function(event) {
-    event.preventDefault();
+    event.preventDefault(); // Prevent form submission
 
-    if (!validateForm()) {
+    if (!validateForm()) { // Validate user input
       return;
     }
 
@@ -10,11 +10,13 @@ $(document).ready(function() {
     const protein = $('#protein').val();
     const fat = $('#fat').val();
 
+    // Perform calculations
     const caloriesProtein = protein * 4;
     const caloriesFat = fat * 9;
     const alternativeGrams = (caloriesProtein + caloriesFat) / 10;
     const recommendedDose = alternativeGrams / factor;
 
+    // Update result fields
     $('#calories-protein').val(caloriesProtein);
     $('#calories-fat').val(caloriesFat);
     $('#alternative-grams').val(alternativeGrams);
